ffa63edce3d9c59886f3f40aa48e57b3e609d919
norvca <norvca@gmail.com>
2019-08-01 15:02:43 +0800

    add API function: aggregation pipeline: unwind, project

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const tourController = require('../controller/tourController');
 
 router.route('/tour-stats').get(tourController.getTourStats);
+router.route('/monthly-plan/:year').get(tourController.getMonthlyPlan);
 
 router
   .route('/top-5-cheap')
